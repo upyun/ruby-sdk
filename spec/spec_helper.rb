9db166e28ed44d01c4c5b64dd8b5b70df14f13f5
jsvisa <delweng@gmail.com>
2014-11-17 20:30:07 +0800

    Update: kick out useless code in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,1 @@
-require 'rubygems'
-require 'rspec'
-require 'bundler/setup'
-
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-
 require 'upyun'
